afac0ac71898efb99c3b605db7155e964b1f76d1
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2022-09-01 10:40:36 -0300

    add

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { TypedResquestBody } from "../types/TypedRequestBody";
 import { User, UserModel } from "../models/user";
 import UnsupportedStoreData from "../exceptions/UnsupportedStoreData";
+import validator from "validator";
 
 export class Service {
     public static async store(req: TypedResquestBody<UserModel>, res: Response, next: NextFunction) {
@@ -9,19 +10,24 @@ export class Service {
             const { email, password } = req.body;
 
             console.log(req.body)
-            
-            // user find by email
-            
-            if (User.findOne({ email })) return res.json({ message: 'user already exists' })
 
-            if (UserModel.checkEmail(email)) throw UnsupportedStoreData.instance;
-            
+            if (Service.checkEmail(email)) throw UnsupportedStoreData.instance
+            // user find by email
+            if (Service.checkDoubleUser(email)) return res.json({ message: 'user already exists' })
+
             const user = await User.create(req.body);
-            
+
             return user
-        
         } catch (err) {
             next(err);
         }
     }
+
+
+    private static checkDoubleUser = (email: string): boolean => {
+        if (User.findOne({ email })) return true;
+        return false;
+    }
+
+    private static checkEmail = (email: string): boolean => validator.isEmail(email)
 }
